9a46b77e193ec40b0ddfbd511ab8e6693aa8216d
Sandrala0413 <sandrala5722@gmail.com>
2024-05-15 09:39:25 +0800

    commit for ch1

---
+++ src/routes/gameMenu/+page.svelte
@@ -0,0 +1,127 @@
+<script>
+  import Header from "../Header.svelte";
+</script>
+
+<svelte:head>
+  <title>Game Menu</title>
+  <meta name="description" content="About this app" />
+</svelte:head>
+<Header>
+  <span><a href="./" style="color: azure;"><i class="fa-solid fa-house"></i></a></span>
+</Header>
+
+<section>
+  <div id="container">
+    <div id="title_container">
+      <div>遊戲選單</div>
+    </div>
+
+    <div id="button_container_top">
+      <div id="game_all" class="game">
+        <div>全關卡</div>
+        <div>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+        </div>
+        <div>尚未遊玩</div>
+      </div>
+
+      <a id="game_1" class="game" href="/gameStart">
+        <div>關卡 1</div>
+        <div>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+        </div>
+        <div>最佳成績</div>
+      </a>
+
+      <a id="game_2" class="game" href="/gameChapterII">
+        <div>關卡 2</div>
+        <div>
+          <i class="fa-regular fa-star"></i>
+          <i class="fa-regular fa-star"></i>
+          <i class="fa-regular fa-star"></i>
+        </div>
+        <div>最佳成績</div>
+      </a>
+    </div>
+
+    <div id="button_container_down">
+      <a id="game_3" class="game" href="/gameChapterIII">
+        <div>關卡 3</div>
+        <div>
+          <i class="fa-regular fa-star"></i>
+          <i class="fa-regular fa-star"></i>
+          <i class="fa-regular fa-star"></i>
+        </div>
+        <div>尚未遊玩</div>
+      </a>
+
+      <a id="game_4" class="game" href="/gameChapterIX">
+        <div>關卡 4</div>
+        <div>
+          <i class="fa-regular fa-star"></i>
+          <i class="fa-regular fa-star"></i>
+          <i class="fa-regular fa-star"></i>
+        </div>
+        <div>失敗</div>
+      </a>
+
+      <a id="game_5" class="game" href="/gameChapterX">
+        <div>關卡 5</div>
+        <div>
+          <i class="fa-regular fa-star"></i>
+          <i class="fa-regular fa-star"></i>
+          <i class="fa-regular fa-star"></i>
+        </div>
+        <div>尚未遊玩</div>
+      </a>
+    </div>
+  </div>
+</section>
+
+<style>
+  section {
+    background-image: url("/src/lib/images/menu_bg.jpg");
+  }
+
+  #container {
+    background-color: azure;
+    /* opacity: 0.8; */
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  #title_container {
+    display: flex;
+    justify-content: center;
+    font-size: 30px;
+    margin-top: 20px;
+  }
+
+  #button_container_top {
+    display: flex;
+  }
+
+  #button_container_down {
+    display: flex;
+  }
+
+  .game {
+    background-color: rgb(254, 254, 196);
+    margin: 30px;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    border-radius: 20px;
+  }
+
+  .fa-solid.fa-star,
+  .fa-regular.fa-star {
+    color: gold;
+  }
+</style>
---
+++ src/routes/gameStart/+page.svelte
@@ -0,0 +1,90 @@
+<script>
+    import Header from "../Header.svelte";
+    const text = [
+        "某日，你收到了一封密信，隱藏在背後的那",
+        "個人為了考驗你的真實實力，在信中用了一",
+        "個特別的加密方式，提示了你......",
+    ];
+</script>
+
+<svelte:head>
+    <title>Game</title>
+    <meta name="cover" content="cover of game" />
+</svelte:head>
+<Header>
+    <a href="./" style="color: azure;">
+        <i class="fa-solid fa-sliders"></i>
+    </a>
+    <div style="width: 95%;"></div>
+    <a href="./" style="color: azure;"><i class="fa-solid fa-house"></i></a>
+</Header>
+
+<section>
+    <div id="container">
+        <div class="typewriter">
+            {#each text as t, i}
+                <p style="animation-delay: {i * 2}s;">{t}</p>
+            {/each}
+        </div>
+        <div id="lock" style="float: right; margin:5px">
+            <a href="/gameChapterIX-2" style="color:azure;">打開密信</a>
+        </div>
+    </div>
+</section>
+
+<style>
+    #container {
+        background-color: rgba(0, 0, 0, 0.6);
+        padding: 60px;
+    }
+    section {
+        background-image: url("/src/lib/images/P1-Paper.jpg");
+        margin: 0;
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        z-index: -1;
+    }
+
+    #lock {
+        color: azure;
+        background-color: cornflowerblue;
+        border-radius: 50px;
+        width: 80px;
+        padding: 10px;
+        font-size: 20px;
+    }
+
+    .typewriter p {
+        color: aliceblue;
+        height: 25px;
+        overflow: hidden;
+        font-size: 20px;
+        word-break: break-all;
+        margin: 10px;
+        visibility: hidden;
+        animation:
+            typing 2s steps(40, end),
+            appear 1s forwards;
+    }
+
+    /* The typing effect */
+    @keyframes typing {
+        0% {
+            width: 0;
+        }
+        100% {
+            width: 100%;
+        }
+    }
+    @keyframes appear {
+        0% {
+            visibility: hidden;
+        }
+        100% {
+            visibility: visible;
+        }
+    }
+</style>
