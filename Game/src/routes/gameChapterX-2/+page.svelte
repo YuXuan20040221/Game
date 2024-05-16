32343fa6cdae0ed30411b34eecbb426c09edd632
NINI <cinnielu47@gmail.com>
2024-05-16 17:10:43 +0800

    first_ch5

---
+++ Game/src/routes/gameChapterX-2/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+    import Header from "../Header.svelte";
+  </script>
+  
+  <Header>
+    <a href="/gameMenu" style="color: white;">
+      <i class="fa-solid fa-sliders"></i>
+    </a>
+    <div style="width: 95%;"></div>
+    <a href="./" style="color: white;">
+      <i class="fa-solid fa-house"></i>
+    </a>
+  </Header>
+  
+  <section>
+  
+  </section>
+  
+  <style>
+    section {
+      background-image: url("/src/lib/images/P5.jpg");
+      /* opacity: 0.5; */
+    }
+  </style>
+  
--- Game/src/routes/gameChapterX/+page.svelte
+++ Game/src/routes/gameChapterX/+page.svelte
@@ -1,7 +1,22 @@
 <script>
-    import Header from "../Header.svelte";
-</script>
-
-<Header>
-    <span><a href="./" style="color: white;"><i class="fa-solid fa-house"></i></a></span>
-</Header>
+    import Anime from "../Anime.svelte";
+    const anm = {
+      text: [
+        "逃離後，你帶著財寶回到家，發現家中好像有人進入過，",
+        "在翻找一番後，你發現並未有東西遺失，但卻多了一封信件， ",
+        "你發現了熟悉的章印，得知這封信應該就是當時告知你寶藏訊息的人，",
+        "但這次卻不是使用上次的加密方式。"
+      ],
+      bgimg: "/src/lib/images/P5-House.jpg",
+      Src: "/gameChapterX-2",
+      btnText: "點擊信件",
+    };
+  </script>
+  
+  <svelte:head>
+    <title>Game</title>
+    <meta name="cover" content="cover of game" />
+  </svelte:head>
+  
+  <Anime {...anm} />
+  
