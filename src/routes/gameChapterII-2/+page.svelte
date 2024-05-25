22d53afdc62f0be9c6ee483b88eee8cc09e57e0b
Sandrala0413 <sandrala5722@gmail.com>
2024-05-25 10:03:11 +0800

    commit for ch2

--- src/routes/gameChapterII-2/+page.svelte
+++ src/routes/gameChapterII-2/+page.svelte
@@ -1,30 +1,15 @@
 <script>
     import Header from "../Header.svelte";
-    const text = [
-        "三人帶著一絲絲的不確定，來到寶藏所在地，",
-        "發現寶藏已被重重機關保護。一旦沒有防備",
-        "地去觸發機關，大家恐怕都要在這個地方喪",
-        "命，此時飛燕大叫了一聲，只見她手裡的密",
-        "信背面，在他的反覆摩擦後竟隱約浮現出一",
-        "行小字，似乎警告著甚麼......"
-    ];
+    import {onMount} from "svelte"
+    import 'image-map-resizer';
 
-    var animationCompleted = false;
-
-    var input = "";
-
-    var mail_text = "村中無水，皆往古月旁汲，日夜不停";
-
-    function openMail(){
-        // @ts-ignore
-        document.getElementById('mail').style.display = 'block';
-        // @ts-ignore
-        document.getElementById('mask').style.display = 'block';
+    function commit(){
+        alert("aaa");
     }
 
-    function closeMail(){
-        window.location.href = "/gameChapterII-2";
-    }
+    onMount(() => {
+        $('map').imageMapResize();
+    });
 
 </script>
 
@@ -41,28 +26,21 @@
 </Header>
 
 <section>
-    <div id="container">
-       <h1>hhh</h1>
-        
+    <div>
+       <img src="/src/lib/images/P2-Map.jpg" usemap="#img_map" id="map" alt="">
+       <map name="img_map">
+            <!-- <area shape="polygon" coords="330, 380, 340, 380, 360, 380, 380, 370, 410, 370, 430, 370, 450, 370, 480, 370, 500, 370, 530, 380, 550, 380, 570, 390, 550, 400, 520, 400, 500, 400, 480, 410, 440, 410, 410, 410, 380, 400, 350, 400" href="https://chatgpt.com/?oai-dm=1"> -->
+            <area shape="rect" coords="250, 300, 650, 550" href="https://chatgpt.com/?oai-dm=1">
+       </map>     
     </div>
  
 </section>
 
 <style>
-    #container {
-        background-color: rgba(0, 0, 0, 0.6);
-        padding: 60px;
-        z-index: 1;
-    }
+
     section {
-        background-image: url("/src/lib/images/P2-Start.jpg");
-        margin: 0;
-        position: fixed;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        z-index: -1;
+        /* background-image: url("");
+        background-size: cover; */
     }
 
     button {
@@ -77,77 +55,12 @@
         outline: none;
     }
 
-    .typewriter p {
-        color: aliceblue;
-        height: 25px;
-        overflow: hidden;
-        font-size: 20px;
-        word-break: break-all;
-        margin: 10px;
-        visibility: hidden;
-        animation:
-            typing 2s steps(40, end),
-            appear 1s forwards;
-    }
 
-    #mask{
-        display: none;
-        background-color: rgba(0, 0, 0, 0.6);
-        width: 100vw;
-        height: 100vh;
-        position: absolute;
+    #map{
+        position: fixed;
         top: 0;
         left: 0;
-        z-index: 2;
-        
+        width: 100vw;
     }
 
-    #mail{
-        display: none;
-        position: absolute;
-        z-index: 3;
-        width: 29%;
-        height: 75%;
-        background-repeat: no-repeat;
-        background-image: url("/src/lib/images/P5-Mail.png");
-    }
-
-    #mail_text{
-        font-size: 15px;
-        font-weight: bold;
-        color: rgba(4, 3, 0, 0.488); 
-
-    }
-
-    #mail_container{
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        height: 100%;
-        justify-content: center;
-    }
-
-    #mail_container button{
-        position: relative; 
-        top: 20px;
-        font-size: 15px;
-    }
-
-    /* The typing effect */
-    @keyframes typing {
-        0% {
-            width: 0;
-        }
-        100% {
-            width: 100%;
-        }
-    }
-    @keyframes appear {
-        0% {
-            visibility: hidden;
-        }
-        100% {
-            visibility: visible;
-        }
-    }
 </style>
